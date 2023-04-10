65abd43eea4f0593f570e415185d1093b410e14f
kamaniyaP <kamaneeyp@gmail.com>
2023-04-11 00:10:36 +0530

    Updating Search bar spacing

--- components/Search.js
+++ components/Search.js
@@ -43,7 +43,7 @@ const Search = () => {
         <div>
             <div className="flex text-justify justify-center">
                 <div className="flex flex-col bg-white rounded-2xl shadow-2xl m-20">
-                    <div className="flex items-center space-x-2 justify-center ml-60 mr-64 md-10">
+                    <div className="flex items-center space-x-2 justify-center mt-2 ml-60 mr-64 mb-2">
                         <div className="absolute top-3 left-3 items-center" ref={clickPoint}>
                             <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                         </div>
